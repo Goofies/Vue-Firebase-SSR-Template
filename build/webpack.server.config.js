84f2667b1c0a7cd57a6f567da299d370a01e4788
Lutfu Can KAPLAN <lutfucankaplan@gmail.com>
2017-11-11 17:25:51 +0300

    moved server to functions folder

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -4,6 +4,7 @@ const base = require('./webpack.base.config')
 const nodeExternals = require('webpack-node-externals')
 const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
 
+
 module.exports = merge(base, {
   target: 'node',
   devtool: '#source-map',
